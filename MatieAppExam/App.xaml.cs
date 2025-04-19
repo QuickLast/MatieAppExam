93f81bd44361536410a92c73de9cd8c029626221
202113 <202113@1305-004>
2025-04-19 11:09:21 +0300

    Добавьте файлы проекта.

---
+++ MatieAppExam/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using MatieAppExam.DB;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MatieAppExam
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static MatieDatabaseExamEntities1 db = new MatieDatabaseExamEntities1();
+    }
+}
---
+++ MatieAppExam/Pages/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using MatieAppExam.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatieAppExam.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            ServiceCosplayLV.ItemsSource = App.db.Service.Where(x => x.Name.StartsWith("Создание косп")).ToList();
+            ServiceCustomLV.ItemsSource = App.db.Service.Where(x => x.Name.StartsWith("Кастомиз")).ToList();
+        }
+
+        private void SortBtn_Click(object sender, RoutedEventArgs e)
+        {
+            TabItem tabItem = ServiceTabCtrl.SelectedItem as TabItem;
+            List<Service> services;
+            if (tabItem.Header.ToString() == "Косплей")
+            {
+                services = App.db.Service.Where(x => x.Name.StartsWith("Создание косп")).ToList();
+                ServiceCosplayLV.ItemsSource = services.OrderByDescending(x => x.Name).ToList();
+            }
+            else if (tabItem.Header.ToString() == "Кастом")
+            {
+                services = App.db.Service.Where(x => x.Name.StartsWith("Кастомиз")).ToList();
+                ServiceCustomLV.ItemsSource = services.OrderByDescending(x => x.Name).ToList();
+            }
+
+        }
+    }
+}
